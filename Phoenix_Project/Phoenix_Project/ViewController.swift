33c0e9ff4d20b3f73c4237e99a33d225de11a09c
hjkim0905 <eax9952@gmail.com>
2024-05-24 22:03:36 +0900

    240524 현중 : 프로젝트 생성

---
+++ Phoenix_Project/Phoenix_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Phoenix_Project
+//
+//  Created by 김현중 on 5/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
